00a28438e40a7eeedb0603c1a1d53dbed106a8c7
msfukui <msfukui@gmail.com>
2023-11-21 18:10:53 +0900

    ブラウザでの動作確認のため Recipe に StarRating を仮組み込み, 色の判定ロジックを実装

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
+import StarRating from "./StarRating";
 
 export default function Recipe({ name, ingredients, steps }) {
   return (
@@ -8,6 +9,7 @@ export default function Recipe({ name, ingredients, steps }) {
       <h1>{name}</h1>
       <IngredientsList list={ingredients} />
       <Instructions title="Cooking Instructions" steps={steps} />
+      <StarRating />
     </section>
   );
 }
