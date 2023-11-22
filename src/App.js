467eeb1c021644e00d45bbfc44d343c30ba849a5
msfukui <msfukui@gmail.com>
2023-11-23 06:29:00 +0900

    6.2.2 の色毎の削除ボタンを実装

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import colorData from "../data/color.json";
 import ColorList from "./components/ColorList.js";
 
 export default function App() {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  const [colors, setColors] = useState(colorData);
+  return (
+    <ColorList
+      colors={colors}
+      onRemoveColor={id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  );
 }
