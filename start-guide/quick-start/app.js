1cb722e4f50a206fd923dd6b883ccc9aaa068e1b
msfukui <msfukui@gmail.com>
2025-07-20 19:46:50 +0900

    feat: About ページの表示を追加

--- start-guide/quick-start/app.js
+++ start-guide/quick-start/app.js
@@ -2,11 +2,21 @@ function MyButton() {
   return <button>I'm a button</button>;
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  )
+}
+
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
